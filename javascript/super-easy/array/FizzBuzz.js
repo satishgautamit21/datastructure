dcf38c24ac88085c8d3305ff6c45fa083c5c9069
satish <satish549321@gmail.com>
2026-08-05 11:07:28 +0530

    more java code added

---
+++ javascript/super-easy/array/FizzBuzz.js
@@ -0,0 +1,17 @@
+function ListFizzBuzz(n) {
+    const list = [];
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            list.push("FizzBuzz");
+        } else if (i % 3 === 0) {
+            list.push("Fizz");
+        } else if (i % 5 === 0) {
+            list.push("Buzz");
+        } else {
+            list.push(i);
+        }
+    }
+    return list;
+}
+
+console.log(ListFizzBuzz(15));
